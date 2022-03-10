9ddb25159cd4cf4aea7f7b284f0e9d2329f9b417
UTROUA1 <andrew.trousilbagan@pearson.com>
2022-03-09 22:57:11 -0600

    table

--- pkg/scylla/db/tables.go
+++ pkg/scylla/db/tables.go
@@ -1,12 +1,1 @@
 package db
-
-import "github.com/scylladb/gocqlx/v2/table"
-
-var TableRules = table.New(RulesMetaData)
-
-var RulesMetaData = table.Metadata{
-	Name:    "rules",
-	Columns: []string{"ruleId", "timestamp"},
-	PartKey: []string{"ruleId"},
-	SortKey: []string{},
-}
