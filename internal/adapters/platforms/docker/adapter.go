714a3b67d31ff789b537b4afd68dd13ad1e0d748
Andrew Bagan <andrew.bagan-2@corteva.com>
2025-03-11 11:29:52 -0500

    docker adapter for vip

---
+++ internal/adapters/platforms/docker/adapter.go
@@ -0,0 +1,59 @@
+// Package docker defines an adapter for Docker.
+//
+// Copyright (c) 2025, The Butler Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package docker
+
+import (
+	"butler/internal/adapters/exec"
+	"context"
+	"go.uber.org/zap"
+)
+
+// DockerAdapter provides a high-level interface for Docker operations.
+type DockerAdapter struct {
+	client *DockerClient
+	logger *zap.Logger
+}
+
+// NewDockerAdapter initializes the adapter.
+func NewDockerAdapter(execAdapter exec.ExecAdapter, logger *zap.Logger) *DockerAdapter {
+	return &DockerAdapter{
+		client: NewDockerClient(execAdapter, logger),
+		logger: logger,
+	}
+}
+
+// GenerateKubeVipManifest generates the Kube-Vip manifest and saves it to a file.
+func (d *DockerAdapter) GenerateKubeVipManifest(ctx context.Context, version, interfaceName, vip string) error {
+	d.logger.Info("Generating Kube-Vip manifest",
+		zap.String("version", version),
+		zap.String("interface", interfaceName),
+		zap.String("vip", vip),
+	)
+	err := d.client.GenerateKubeVipManifest(ctx, version, interfaceName, vip)
+	if err != nil {
+		d.logger.Error("Failed to generate Kube-Vip manifest",
+			zap.Error(err),
+			zap.String("version", version),
+			zap.String("interface", interfaceName),
+			zap.String("vip", vip),
+		)
+		return err
+	}
+
+	d.logger.Info("Successfully generated Kube-Vip manifest")
+	return nil
+}
