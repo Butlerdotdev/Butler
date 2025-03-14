2ec9b139931e9373e7133b4f1309ec17922c6143
Andrew Bagan <andrew.bagan-2@corteva.com>
2025-03-13 19:15:13 -0500

    flux adapter init

---
+++ internal/adapters/platforms/flux/adapter.go
@@ -0,0 +1,36 @@
+// Package flux defines an adapter for executing flux commands.
+//
+// Copyright (c) 2025, The Butler Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the Apache License, Version 2.0 (the "License");
+// limitations under the License.
+
+package flux
+
+import (
+	"butler/internal/adapters/exec"
+	"context"
+	"go.uber.org/zap"
+)
+
+// FluxAdapter provides a high-level interface for interacting with Kubernetes via flux.
+type FluxAdapter struct {
+	client *FluxClient
+}
+
+// NewFluxAdapter initializes a new KubectlAdapter.
+func NewFluxAdapter(execAdapter exec.ExecAdapter, logger *zap.Logger) *FluxAdapter {
+	return &FluxAdapter{client: NewFluxClient(execAdapter, logger)}
+}
+
+// ExecuteFluxCommand runs a generic kubectl command with provided arguments.
+func (f *FluxAdapter) ExecuteCommand(ctx context.Context, args ...string) (string, error) {
+	return f.client.ExecuteCommand(ctx, args...)
+}
